9630384d630b4faa1faa316cac608950bff618bf
Jenna Bols <jennabols1@gmail.com>
2023-02-24 20:17:11 -0500

    I have added a function that introduces a binary search to find if a number lies within a given array.

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	if (low > high)
+	{
+		return -1;
+	}
+
+    // find the inital middle value
+	int mid_val = (low + high) / 2;
+
+    // case if middle value is greater than the intended target
+    if (numbers[mid_val] > value)
+    {
+        // the low value remains; the middle value changes by one element
+        return search(numbers, low, mid_val - 1, value);
+    }
+
+    // case if the middle value is less than the intended target
+    else if (numbers[mid_val] < value)
+	{
+        // the high value remains; the middle value changes by one element
+	    return search(numbers, mid_val + 1, high, value);
+	}
+
+    // case if the middle value is equivalent to the intended target
+    else 
+    {
+        return mid_val;
+    }
 }
 
 void printArray(int numbers[], int sz)
